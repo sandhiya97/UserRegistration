4e84dec6c07001e972281b64dbea62aae8f0d2be
sandhiya97 <sandhiya.baburao@gmail.com>
2022-03-17 07:51:44 +0530

    To check parameterized multiple emails, a new class has been created.

---
+++ src/UserRegistration/UserValidationTest.java
@@ -0,0 +1,69 @@
+package UserRegistration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserValidationTest {
+
+    UserValidation user = new UserValidation();
+
+    @org.junit.jupiter.api.Test
+    public void testFirstName_thenAssertionSucceeds() {
+            assertTrue(user.isFirstNameValid("Naveen"));
+            System.out.println("Valid First Name");
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testFirstName_thenAssertionFail() {
+        assertFalse(user.isFirstNameValid("naveen"));
+        System.out.println("InvalidValid First Name");
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testLastName_thenAssertionSucceeds() {
+        assertTrue(user.isLastNameValid("Kumar"));
+        System.out.println("Valid Last Name");
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testLastName_thenAssertionFail() {
+        assertFalse(user.isLastNameValid("kumar"));
+        System.out.println("Invalid Last Name");
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testEmail_thenAssertionSucceeds() {
+        assertTrue(user.isEmailValid("abc@gmail.com"));
+        System.out.println("Valid Email");
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testEmail_thenAssertionFail() {
+        assertFalse(user.isEmailValid("abc@gmail@.com@"));
+        System.out.println("Invalid Email");
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testMobileNumber_thenAssertionSucceeds() {
+        assertTrue(user.isPhoneNumberValid("91 9876543210"));
+        System.out.println("Valid Mobile Number");
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testMobileNumber_thenAssertionFail() {
+        assertFalse(user.isPhoneNumberValid("910876543210"));
+        System.out.println("Invalid Mobile Number");
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testPassword_thenAssertionSucceeds() {
+        assertTrue(user.isPasswordValid("Abcd@1234"));
+        System.out.println("Valid Password");
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testPassword_thenAssertionFail() {
+        assertFalse(user.isPasswordValid("bbb"));
+        System.out.println("Invalid Password");
+    }
+
+}
